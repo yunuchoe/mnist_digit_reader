6797bccd7d78235025322c36bb9dd2671cc09145
Yunu Choe <yunuchoe10@gmail.com>
2025-09-27 21:45:03 -0700

    first commit

---
+++ image_app/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+
+from torch_utils import transform_image, get_prediction
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # check POST - only one we care about for this project
+    if request.method == 'POST':
+        #variables
+        file = request.files.get('file') # file itself
+        filename = file.filename.lower() # filename
+
+        # error check
+        if file is None: # no file
+            return jsonify({'error': 'no file found'})
+        elif filename == "": # no file name
+            return jsonify({'error': 'no file name'})
+        elif not filename.lower().endswith(('.png', '.jpg', '.jpeg')): # file type must end in .png .jpg .jpeg
+            return jsonify({'error': 'illegal file format'})
+        
+        try:
+            image = file.read()
+            tensor = transform_image(image) # convert to tensor
+            predicted, confidence = get_prediction(tensor) # predict
+
+            result = { 'prediction': predicted, 'confidence': confidence}
+            return jsonify(result)
+        except:
+            return jsonify({'error': 'error during predictions'})
---
+++ image_app/torch_utils.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import io
+
+from PIL import Image
+
+# get an image
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.input_size = input_size
+        self.l1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)  
+    
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        
+        return out
+    
+# Hyper-parameters 
+input_size = 784 #28x28 pixels size of MNIST
+hidden_size = 500 
+num_classes = 10
+
+model = NeuralNet(input_size, hidden_size, num_classes)
+
+PATH = "mnist_ffn.pth" # previous training
+model.load_state_dict(torch.load(PATH))
+model.eval()
+
+# convert raw image bytes to tensor
+def transform_image(image_bytes):
+    # single chanel
+    # image 28x28 as outlined above
+    # convert to PyTorch tensor
+    # mean / standard of dev.
+    transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.Resize((28, 28)), transforms.ToTensor(), transforms.Normalize((0.1307,),(0.3081,))])
+
+    image = Image.open(io.BytesIO(image_bytes))
+
+    return transform(image).unsqueeze(0)
+
+# predict
+def get_prediction(image_tensor):
+    images = image_tensor.reshape(-1, 28*28) # resahpe to 28x28
+    output = model(images)
+    
+    probabilities = torch.softmax(output, dim = 1)
+    confidence, predicted = torch.max(probabilities, 1)
+
+    return predicted.item(), confidence.item()
